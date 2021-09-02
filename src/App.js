69c921d853c26fc97bec6dfaa0dc97cf3347b414
DConstantine9 <constantinedeniskkin@gmail.com>
2021-09-02 23:22:04 +0300

    design and lading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import './App.css';
+import './Search.css';
+import './Card.css';
+
 
 function App() {
   const [inputValue, setInputValue] = React.useState("")
+  const [isLoading, setIsLoading] = React.useState(false)
   const [repos, setRepos] = React.useState([])
   const [error, setError] = React.useState(false)
 
@@ -11,15 +15,19 @@ function App() {
       return
     }
 
+    setIsLoading(true)
+
     fetch("https://api.github.com/search/repositories?q=" + inputValue)
       .then(res => {
         return res.json()  
       })
       .then(data => {
         console.log(data)
+        setIsLoading(false)
         setRepos(data.items)
       })
       .catch(err => {
+        setIsLoading(false)
         setError(true)
         console.log(err)
       })
@@ -33,21 +41,35 @@ function App() {
         setInputValue(e.target.elements.query.value)
       }}>
         <div className="App">
-          <div>
-            <input type="text" name="query" placeholder="Search" />
+          <div id="searchBlock">
+            <input type="text" name="query" placeholder="Начните вводить текст для поиска (не менее трех символов)" id="searchBar"/>
+            <button type="submit" id="searchBtn"><i className="fas fa-search"></i></button>
           </div>
         </div>
       </form>
+      {isLoading && <div className="loading">Поиск проектов...</div>}
       {error && (
         <div>
           Fetching data error
         </div>
       )}
-      <div>
+      <div className="cardBlock">
         {repos.map(repo => {
           return (
-            <div key={repo.id}>
+            <div key={repo.id} className="card">
               <a href={repo.html_url}>{repo.name}</a>
+              <div className="info">
+                <img src={repo.owner.avatar_url} alt=""/> 
+                <div>{repo.owner.login} </div>
+              </div>
+              <div className="info"> 
+                <i className="fas fa-star"></i> <div className="stargazers">{repo.stargazers_count}</div>
+                <i className="fas fa-eye"></i><div>{repo.watchers_count}</div>
+              </div>
+              <div className="comment">
+                <input  />
+                <button><i className="fas fa-pencil-alt"></i></button>
+              </div>
             </div>
           )
         })}
